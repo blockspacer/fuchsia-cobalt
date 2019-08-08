3750d9543dc8d297e59b2401292a84695f2cf19e
Zach Bush <zmbush@google.com>
2019-08-08 13:53:25 -0700

    [cpplint] Lint header guards
    clang-tidy doesn't have the same header guard lint as we've been using
    
    Change-Id: I1dab88ac22750b878100e1e7d1be766210b76780

--- tools/cpplint.py
+++ tools/cpplint.py
@@ -26,6 +26,8 @@ SKIP_LINT_DIRS = [
     os.path.join(SRC_ROOT_DIR, 'out'),
     os.path.join(SRC_ROOT_DIR, 'sysroot'),
     os.path.join(SRC_ROOT_DIR, 'third_party'),
+    os.path.join(SRC_ROOT_DIR, 'config', 'config_parser', 'src',
+                 'source_generator', 'source_generator_test_files'),
 ]
 
 TEST_FILE_REGEX = re.compile('.*_(unit)?tests?.cc$')
@@ -33,6 +35,55 @@ TEST_FILE_CLANG_TIDY_CHECKS = [
     '-readability-magic-numbers',
     '-misc-non-private-member-variables-in-classes'
 ]
+
+
+def report_error(filename, linenum, message):
+  print('%s:%s: %s' % (filename, linenum, message))
+
+
+# TODO(zmbush): try to accomplish this with llvm-header-guard
+def has_guard(f):
+  if os.path.islink(f):
+    return True
+
+  localpath = os.path.relpath(f, SRC_ROOT_DIR)
+  guard = 'COBALT_%s_' % re.sub(r'[-/.]', '_', localpath).upper()
+  ifndef = ''
+  ifndef_linenum = 0
+  define = ''
+  endif = ''
+  endif_linenum = 0
+  with open(f) as fd:
+    for linenum, line in enumerate(fd):
+      linesplit = line.split()
+      if len(linesplit) >= 2:
+        if not ifndef and linesplit[0] == '#ifndef':
+          ifndef = linesplit[1]
+          ifndef_linenum = linenum
+        if not define and linesplit[0] == '#define':
+          define = linesplit[1]
+      if line.startswith('#endif'):
+        endif = line
+        endif_linenum = linenum
+
+  if not ifndef or not define or ifndef != define:
+    report_error(
+        localpath, 0,
+        'No #ifndef header guard found, suggested CPP variable is %s' % guard)
+    return False
+
+  if ifndef != guard:
+    report_error(localpath, ifndef_linenum,
+                 '#ifndef header guard has wrong style, please use %s' % guard)
+    return False
+
+  endif_re = re.compile(r'#endif\s*//\s*' + guard + r'\b')
+  if endif_re.match(endif):
+    return True
+
+  report_error(localpath, endif_linenum,
+               '#endif line should be "#endif  // %s"' % guard)
+  return False
 
 
 def main(only_directories=[]):
@@ -64,6 +115,10 @@ def main(only_directories=[]):
     ]
     for d in dirs_to_skip:
       dirs.remove(d)
+
+  for f in clang_tidy_files + clang_tidy_test_files:
+    if f.endswith('.h') and not has_guard(f):
+      status += 1
 
   clang_tidy_command = [CLANG_TIDY, '-quiet', '-p', OUT_DIR]
   print('Running clang-tidy on %d source files' % len(clang_tidy_files))
